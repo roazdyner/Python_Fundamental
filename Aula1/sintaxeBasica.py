86cb137c0aaf58a028ee5264a0e9fd3e2e870c0e
roazdyner <rodnei.azzola@gmail.com>
2019-11-25 13:53:32 -0200

    Trabalhando com variaveis

---
+++ Aula1/sintaxeBasica.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+print('Hello World')
+nome=input('Digite seu nome: ')
+#print(nome)
+idade=input('Digite sua idade: ')
+# print (f'{nome}, você tem {idade} anos')
+# print()
+# print ('{}, você tem {} anos'.format(nome,idade))
+
+print(nome,idade,sep='.',end='\n\n')
+
+#print(input('Qual a sua idade: '), 'Anos')
+# print ('Seu nome é', nome)
+# print (f'Seu nome é {nome}')
+# print ('Seu nome é {}'.format(nome))
+
+#Tipos de variaveis
+
+# texto = '' #string
+# numero = 3 #int
+# numero_real = 3.6 #float
+# CONSTANTE = 3.14 # representação em codigo de uma constante
+# booleanas = TRUE # bool -> true or false
+# listas = ['item1','item2','item3'] #listas
+# tuplas = ['item1','item2','item3'] # tuple
+# tuplas2 = 'item1','item2','item3' #tupla
+# dicionarios = {'nome':'Rodnei','Sobrenome':'Azzola'} #dict

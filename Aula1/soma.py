aa9f79daba50d3f11facd29b91e67b17d8147ca6
roazdyner <rodnei.azzola@gmail.com>
2019-11-25 21:05:24 -0300

    soma.py

---
+++ Aula1/soma.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+mensagem1 = 'Digite o primeiro numero: '
+num1 = float(input(mensagem1))
+
+mensagem2 = 'Digite o segundo numero: '
+num2 = float(input(mensagem2))
+
+print(f'O resultado da soma é: {num1 + num2}')
